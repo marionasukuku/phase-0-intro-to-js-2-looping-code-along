dc19c74c26830a8f156848bf9c6b312acb15e019
Marion <marionasukuku22@gmail.com>
2022-11-28 09:00:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const writeCards = (array,surprise)=>{
+    let emptyArray =[]
+    let item = "";
+
+    for (item of array){
+         emptyArray.push( `Thank you, ${item}, for the wonderful ${surprise} gift!`) 
+    }
+    return emptyArray
+
+}
+
+const countDown = (number)=>{
+    for (let i = number;i>=0;i--){
+        console.log(i)
+    }
+}
